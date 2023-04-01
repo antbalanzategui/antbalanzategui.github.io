3f8c9172858ab88ac5327bc6cc3d5bdc05b5f8ba
Antonio Balanzategui <antbalanzategui@vt.edu>
2023-04-01 00:31:46 -0400

    Added success page and rain

--- script.js
+++ script.js
@@ -80,6 +80,37 @@ if (localStorage.getItem('theme') === 'dark') {
     document.getElementById('pImage6').src = './utils/lightMode/lightModeD3JS.png';
     }
   }
+  var makeItRain = function() {
+    // clear out everything inside the rainHolder div
+    $('.rainHolder .rain').empty();
+  
+    // rest of the function remains the same
+    var increment = 0;
+    var drops = "";
+    var backDrops = "";
+  
+    while (increment < 100) {
+      //couple random numbers to use for various randomizations
+      //random number between 98 and 1
+      var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+      //random number between 5 and 2
+      var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+      //increment
+      increment += randoFiver;
+      //add in a new raindrop with various randomizations to certain CSS properties
+      drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+      backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+    }
+  
+    $('.rainHolder .rain.front-row').append(drops);
+    $('.rainHolder .rain.back-row').append(backDrops);
+  }
+  
+  if (window.location.href.includes("rain.html")) {
+    $('body').addClass('splat-toggle');
+    $('body').addClass('back-row-toggle');
+    makeItRain();
+  }
 
 
 
